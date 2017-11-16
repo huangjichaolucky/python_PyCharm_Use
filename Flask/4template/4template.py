797cf6dcf6e1e6e874a684dc686398fe6bab2837
huangjichao <huangjichao@qq.com>
2017-11-16 11:49:59 +0800

    数据库章节，暂没有具体实践

--- Flask/4template/4template.py
+++ Flask/4template/4template.py
@@ -1,10 +1,17 @@
-#-*- coding:utf-8 -*-
+#-*- coding=utf-8 -*-
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
-
 @app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/login/')
+def login():
+    return render_template('login.html')
+
+@app.route('/helloWorld')
 def hello_world():
 
     # return 'Hello World!'
@@ -27,7 +34,7 @@ def hello_world():
     userInfo = {
         'username' : u'小明',
         'sex' : u'男',
-        'age' : 30,
+        'age' : 50,
         'person': p,
         'tempDic': {
             't1':'t1Value',
@@ -41,6 +48,82 @@ def hello_world():
     # 3若传递多个参数，可将多个参数放到字典中去，然后将字典转成关键参数传递过去，
     # 该字典中 的某个键 对应的值可以是基本类型、对象类型、字典类型均可
     return render_template('helloWorldHtml.html', **userInfo)
+
+    # 4 在模板文件中如何使用if 语句，
+    # 情景如下，年纪大于30在模板中显示  大叔， 年纪大于30
+    # 若小于30，显示  小鲜肉
+    # 详见 模板文件
+
+# 访问 ifTest路径查看效果
+@app.route('/ifTest/')
+def ifTest():
+    age = 30
+    return render_template('ifTest.html', age = age)
+
+
+#访问 forTest路径 查看效果
+@app.route('/forTest/')
+def forTest():
+    booksDic = {
+        'name' : u'好书',
+        'price' : 20,
+        'author' : u'好作者'
+    }
+
+    array = [
+        1, 3, 4, 5
+    ]
+    return  render_template('forTest.html',
+                            booksDic = booksDic, array = array)
+
+class Book(object):
+    def __init__(self, name, author, price):
+        self.name = name
+        self.author = author
+        self.price = price
+
+@app.route('/forDemo/')
+def forDemo():
+
+    b1 = Book(u'红楼梦', u'曹雪芹', 300)
+    b2 = Book(u'三国演义', u'罗贯中', 200)
+    b3 = Book(u'西游记', u'吴承恩', 330)
+    b4 = Book(u'水浒传', u'施耐庵', 100)
+    books = [
+        b1, b2, b3, b4
+    ]
+    return  render_template('forDemo.html', books =books)
+
+
+# 过滤器
+@app.route('/filterTest/')
+def filterTest():
+
+    b1 = Book(u'红楼梦', u'曹雪芹', 300)
+    b2 = Book(u'三国演义', u'罗贯中', 200)
+    b3 = Book(u'西游记', u'吴承恩', 330)
+    b4 = Book(u'水浒传', u'施耐庵', 100)
+    books = [
+        b1, b2, b3, b4
+    ]
+    avatar = 'https://www.baidu.com/img/bd_logo1.png'
+    # return render_template('filterTest.html')
+    return render_template('filterTest.html',avatar=avatar, books=books)
+
+#基类视图
+@app.route('/baseView/')
+def baseView():
+    return render_template('baseView.html')
+
+#子类页面1
+@app.route('/subView/')
+def subView():
+    return render_template('subView.html')
+
+#子类页面2
+@app.route('/subView2/')
+def subView2():
+    return render_template('subView2.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ Flask/4template/static/js/alert.js
@@ -0,0 +1,4 @@
+/**
+ * Created by admin on 2017/11/15.
+ */
+alert('heloWorld')
